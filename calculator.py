f4115d02685a219a15d1c2409950eac70bdd4303
gmtx-source <gbezayne@gmail.com>
2025-07-08 11:19:12 -0300

    Added new calculation feature

--- calculator.py
+++ calculator.py
@@ -9,39 +9,56 @@ def invalid_number(number_str):
     
     return False
 
-prompt('This is a calculator')
+### Function new calculation
+def number_calculation():
 
-prompt('Choose a number: ')
-n1 = input()
-
-while invalid_number(n1):
-    prompt("That's an invalid number... Choose another number: ")
+    prompt('This is a calculator')
+    prompt('Choose a number: ')
     n1 = input()
 
-prompt('Choose another number: ')
-n2 = input()
-while invalid_number(n2):
-    prompt("That's an invalid number... Choose another number: ")
+    while invalid_number(n1):
+        prompt("That's an invalid number... Choose another number: ")
+        n1 = input()
+
+    prompt('Choose another number: ')
     n2 = input()
+    while invalid_number(n2):
+        prompt("That's an invalid number... Choose another number: ")
+        n2 = input()
 
-prompt('Choose 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division')
-conta = input()
-
-while conta not in ['1', '2', '3', '4']:
-    prompt('Must choose between "1" and "4"')
+    prompt('Choose 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division')
     conta = input()
 
-match conta:
-    case '1':
-        output = (int(n1) + int(n2))
-    case '2':
-        output = (int(n1) - int(n2))
-    case '3':
-        output = (int(n1) * int(n2))
-    case '4':
-        output = (int(n1) / int(n2))
+    while conta not in ['1', '2', '3', '4']:
+        prompt('Must choose between "1" and "4"')
+        conta = input()
 
-print(f'Result: {output}')
+    match conta:
+        case '1':
+            output = (int(n1) + int(n2))
+        case '2':
+            output = (int(n1) - int(n2))
+        case '3':
+            output = (int(n1) * int(n2))
+        case '4':
+            output = (int(n1) / int(n2))
 
+    print(f'Result: {output}')
+    return output
 
-print('teste A')
+number_calculation()
+
+prompt('Would you like to make another calculation? type y or n')
+newcalculation = input()
+
+while newcalculation not in ['y', 'n']:
+    prompt('Invalid input insert again: ')
+    newcalculation = input()
+
+while newcalculation == 'y':
+    number_calculation()
+    prompt('Would you like to make another calculation? type y or n')
+    newcalculation = input()
+
+if newcalculation == 'n':
+    prompt('THANKS FOR USING THE CALCULATOR')
