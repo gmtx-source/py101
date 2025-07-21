e8d340709c5427dd2ca6fed15d0abbb93c4d8c2c
gmtx-source <gbezayne@gmail.com>
2025-07-21 08:20:47 -0300

    Calculator bonus features final

--- calculator.py
+++ calculator.py
@@ -1,57 +1,66 @@
+LANGUAGE = 'en'
+
 import json
 
 with open('calculator_messages.json', 'r') as file:
     MESSAGES = json.load(file)
 
-def prompt(message):
+#Define the language of the text program
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
+
+def prompt(key):
+    message = messages(key, LANGUAGE)
     print(f"==> {message}")
 
-prompt(MESSAGES['welcome'])
 
 def invalid_number(number_str):
+    
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     
     return False
 
+
 while True:
 
-    prompt('This is a calculator')
-    prompt('Choose a number: ')
+    prompt('welcome')
+    prompt('choose_first_number')
     n1 = input()
 
     while invalid_number(n1):
-        prompt(MESSAGES['invalid_number'])
+        prompt('invalid_number')
         n1 = input()
 
-    prompt('Choose another number: ')
+    prompt('choose_second_number')
     n2 = input()
     while invalid_number(n2):
-        prompt(MESSAGES['invalid_number'])
+        prompt('invalid_number')
         n2 = input()
 
-    prompt('Choose 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division')
+    prompt('choose_operation')
     conta = input()
 
     while conta not in ['1', '2', '3', '4']:
-        prompt('Must choose between "1" and "4"')
+        prompt('invalid_operation')
         conta = input()
 
     match conta:
         case '1':
-            output = (int(n1) + int(n2))
+            output = (float(n1) + float(n2))
         case '2':
-            output = (int(n1) - int(n2))
+            output = (float(n1) - float(n2))
         case '3':
-            output = (int(n1) * int(n2))
+            output = (float(n1) * float(n2))
         case '4':
-            output = (int(n1) / int(n2))
+            output = (float(n1) / float(n2))
 
     print(f'Result: {output}')
 
-    prompt('Would to like to perform another calculation? (y/n)? ')
+    prompt('another_calculation')
     
     answer = input()
     if answer and answer[0].lower() != 'y':
